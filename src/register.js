cad3c5fa7d494427a7442c74818ee258a2efc444
Tomáš Bosák <tombos255@gmail.com>
2024-07-15 23:15:26 +0200

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Axios from 'axios';
-import { StyleSheet, Text, View, TextInput, SafeAreaView, Button } from 'react-native';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button, CheckBox } from 'react-native';
 
 
 function Register({ navigation }) {
 
-    const [text , onChangeText ] = React.useState('');
+    const [text, onChangeText] = React.useState('');
     const [text2, onChangeText2] = React.useState('');
     const [text3, onChangeText3] = React.useState('');
     const [text4, onChangeText4] = React.useState('');
@@ -13,15 +13,29 @@ function Register({ navigation }) {
     const [text6, onChangeText6] = React.useState('');
     const [text7, onChangeText7] = React.useState('');
 
-    const HandlePress = () => {
-        console.log(text);
-        console.log(text2);
-        console.log(text3);
-        console.log(text4);
-        console.log(text5);
-        console.log(text6);
-        console.log(text7);
-        console.log(' - - - ');
+    const [isSelected, setSelection] = useState(false);
+
+
+    const HandleRegistaration = () => {
+
+
+        axios.post("http://0.0.0.0:3000/users/register", {
+            text: text,
+            text2: text2,
+            text3: text3,
+            text4: text4,
+            text5: text5,
+            text6: text6,
+            text7: text7,
+            isSelected: isSelected,
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
     }
 
     return (
@@ -84,9 +98,23 @@ function Register({ navigation }) {
                     placeholder="Heslo znovu"
                 />
 
+                <View style={styles2.container}>
+                    <Text style={styles2.label}>Zustat Prihlaseny</Text>
+                    <CheckBox
+                        value={isSelected}
+                        onValueChange={setSelection}
+                        style={styles2.checkbox}
+                    />
+                </View>
+
+
+
+
+
+
                 <Button
                     title="Registrovat"
-                    onPress={() => HandlePress()}
+                    onPress={() => HandleRegistaration()}
                 />
 
             </SafeAreaView>
@@ -119,7 +147,28 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
     },
+    checkboxContainer: {
+        flexDirection: 'row',
+        marginBottom: 20,
+    },
+    checkbox: {
+        alignSelf: 'center',
+    },
 
 });
+
+const styles2 = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10,
+      },
+      label: {
+        marginRight: 10,
+      },
+      checkbox: {
+        alignSelf: 'center',
+      },
+})
 
 export default Register;
